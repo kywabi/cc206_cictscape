eb9b493bf874488dbae19c3ab6d3f948e8968bef
kylabearneza <124786899+kylabearneza@users.noreply.github.com>
2023-10-05 10:47:30 +0800

    import chronoroom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cc206_cictscape/feature/chronoroom.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Helloo World!'),
-        ),
-      ),
+      title: 'Flutter Demo',
+      home: const ChronoRoom(),
     );
   }
 }
